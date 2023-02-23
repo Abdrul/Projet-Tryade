d809d319c3d50080ab1a7d53cb91230666e99824
Abel <josephine.abel@outlook.fr>
2023-02-23 21:54:26 +0100

    end of parameters

--- index.js
+++ index.js
@@ -26,22 +26,144 @@ decrease.forEach((down, index) => {
   });
 });
 
-const input = document.getElementById("input");
+const div = document.querySelector(".parameters");
+let test = getComputedStyle(div);
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "r") {
+    test.display === "none"
+      ? div.style.setProperty("display", "block")
+      : div.style.setProperty("display", "none");
+  }
+});
+
+const buttonCloseParameters = document.querySelector(".close-parameters");
+
+buttonCloseParameters.addEventListener("click", () => {
+  div.style.setProperty("display", "none");
+});
+
+const toggleHeart = document.getElementById("toggle-heart");
+const toggleFood = document.getElementById("toggle-food");
+const toggleShield = document.getElementById("toggle-shield");
+const toggleThirst = document.getElementById("toggle-thirst");
+const heartContainer = document.querySelector(".heartContainer");
+const foodContainer = document.querySelector(".foodContainer");
+const shieldContainer = document.querySelector(".shieldContainer");
+const thirstContainer = document.querySelector(".thirstContainer");
+
+const toggleArray = [];
+toggleArray.push(toggleHeart, toggleFood, toggleShield, toggleThirst);
+
+const statusArray = [];
+statusArray.push(
+  heartContainer,
+  foodContainer,
+  shieldContainer,
+  thirstContainer
+);
+
+toggleArray.forEach((toggle, index) => {
+  toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+      statusArray[index].style.setProperty("display", "none");
+    } else {
+      statusArray[index].style.setProperty("display", "block");
+    }
+  });
+});
+
+const choiceColors = document.querySelectorAll(".choice-color");
+const rootVariableColor = document.querySelector(":root");
+
+choiceColors.forEach((choiceColor) => {
+  choiceColor.addEventListener("click", (e) => {
+    choiceColors.forEach((o) => o.classList.remove("selected"));
+    choiceColor.classList.add("selected");
+
+    const getBackgroundValue = getComputedStyle(e.target).backgroundColor;
+
+    rootVariableColor.style.setProperty(
+      "--backgroundBarLife",
+      getBackgroundValue
+    );
+  });
+});
+
+const inputColor = document.getElementById("color");
 
 const changeColorAth = () => {
-  const color = input.value;
+  const color = inputColor.value;
   circles.forEach((circle) => {
     circle.style.stroke = color;
   });
 };
 
-input.addEventListener("input", changeColorAth);
+inputColor.addEventListener("input", changeColorAth);
 
-// const div = document.querySelector(".parameters");
+/* ************************************ DRAG *************************************** */
+// Sélectionne tous les éléments ayant la classe "draggableClick"
+const draggableElements = document.querySelectorAll(".draggableClick");
 
-// document.addEventListener("keydown", (event) => {
-//   if (event.key === "r") {
-//     div.style.display = div.style.display === "none" ? "flex" : "none";
-//     // div.classList.toggle("active");
-//   }
-// });
+// Ajoute un écouteur d'événement "mousedown" à chaque élément draggable
+
+draggableElements.forEach((element) => {
+  element.addEventListener("mousedown", startDrag);
+});
+
+function startDrag(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  // Empêche le clic droit de déclencher le drag and drop
+  if (event.button !== 0) return;
+
+  let element = event.target;
+  while (!element.classList.contains("draggable")) {
+    element = element.parentElement;
+  }
+
+  // Stocke l'élément sélectionné
+  let selectedElement = element;
+
+  // Calcule les distances entre le clic de la souris et le bord de l'élément
+  let offsetLeft = event.clientX - element.offsetLeft;
+  let offsetTop = event.clientY - element.offsetTop;
+
+  // Ajoute un écouteur d'événement "mousemove" à la fenêtre pour suivre les mouvements de la souris
+  window.addEventListener("mousemove", dragElement);
+
+  // Fonction pour déplacer l'élément en fonction des mouvements de la souris
+  /**
+   *
+   * @param {MouseEvent} event
+   */
+  function dragElement(event) {
+    // Calcule la nouvelle position de l'élément en fonction de la distance parcourue par la souris
+    let newLeft = event.clientX - offsetLeft;
+    let newTop = event.clientY - offsetTop;
+
+    // Si l'élément sort de la fenêtre, on le bloque
+    if (newLeft < 0) newLeft = 0;
+    if (newTop < 0) newTop = 0;
+    if (
+      newLeft >
+      window.innerWidth /* Storing the element that is being dragged. */ -
+        selectedElement.offsetWidth
+    ) {
+      newLeft = window.innerWidth - selectedElement.offsetWidth;
+    }
+    if (newTop > window.innerHeight - selectedElement.offsetHeight) {
+      newTop = window.innerHeight - selectedElement.offsetHeight;
+    }
+
+    // Déplace l'élément en modifiant ses propriétés CSS "left" et "top"
+    selectedElement.style.left = newLeft + "px";
+    selectedElement.style.top = newTop + "px";
+  }
+
+  // Ajoute un écouteur d'événement "mouseup" à la fenêtre pour détecter quand l'utilisateur relâche le clic de la souris
+  window.addEventListener("mouseup", () => {
+    // Supprime l'écouteur d'événement "mousemove" de la fenêtre pour arrêter le déplacement
+    window.removeEventListener("mousemove", dragElement);
+  });
+}
