c8955d798bd21409cafaf2caad03df655a0c2784
Hababjiour <camstutzcore@gmail.com>
2023-02-23 12:01:41 +0100

    Remettre les boutons

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function (event) {
     //Car ATH
     //The car signals
@@ -247,52 +246,4 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     //End of Car ATH
 });
-=======
-const increase = document.querySelectorAll(".increase");
-const decrease = document.querySelectorAll(".decrease");
 
-const circles = document.querySelectorAll(".another-circle");
-const secondCircles = document.querySelectorAll("#circle-id");
-
-increase.forEach((up, index) => {
-  up.addEventListener("click", () => {
-    let test = parseInt(circles[index].getAttribute("data-stroke"));
-    if (test > 0) {
-      test = test - 5;
-      circles[index].style.strokeDashoffset = test;
-      circles[index].setAttribute("data-stroke", test);
-    }
-  });
-});
-
-decrease.forEach((down, index) => {
-  down.addEventListener("click", () => {
-    let test = parseInt(circles[index].getAttribute("data-stroke"));
-    if (test < 125) {
-      test = test + 5;
-      circles[index].style.strokeDashoffset = test;
-      circles[index].setAttribute("data-stroke", test);
-    }
-  });
-});
-
-const input = document.getElementById("input");
-
-const changeColorAth = () => {
-  const color = input.value;
-  circles.forEach((circle) => {
-    circle.style.stroke = color;
-  });
-};
-
-input.addEventListener("input", changeColorAth);
-
-// const div = document.querySelector(".parameters");
-
-// document.addEventListener("keydown", (event) => {
-//   if (event.key === "r") {
-//     div.style.display = div.style.display === "none" ? "flex" : "none";
-//     // div.classList.toggle("active");
-//   }
-// });
->>>>>>> f33aa8ddc7f96a4a541092bd1be0eeb9a9ae3533
